685df6bd455d6e0900d8d9d5ba9845783752952c
Venkat <venkygunturu725@gmail.com>
2026-07-23 12:17:51 +0530

    ssl configuration in database.js

--- config/database.js
+++ config/database.js
@@ -2,14 +2,21 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        logging: false, 
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
 );
 
 module.exports = sequelize;
