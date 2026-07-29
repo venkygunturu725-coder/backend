19e693c4e44a211d6077fdea4d032bbee1c9655e
Venkat <venkygunturu725@gmail.com>
2026-07-29 15:25:03 +0530

    dummy commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/submissions', submissionRoutes);
 app.use('/api/admin', adminRoutes);
 
-//swagger setup
+//swagger setup 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
