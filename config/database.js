1f36dbec4633227e6f13219fdd9ea4df71d295b1
Venkat <venkygunturu725@gmail.com>
2026-07-29 16:20:29 +0530

    updating database config

--- config/database.js
+++ config/database.js
@@ -1,25 +1,21 @@
 const { Sequelize } = require('sequelize');
 const pg = require('pg');
 
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectModule: pg,
-    logging: false,
-
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
     },
-  }
-);
+    pool: {
+        max: 3,
+        min: 0,
+        idle: 10000,
+        acquire: 30000
+    }
+});
 
 module.exports = sequelize;
