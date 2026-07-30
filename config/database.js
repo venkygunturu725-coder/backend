90044adcd0f951c8ef55872f00c10c48047ad1f8
Venkat <venkygunturu725@gmail.com>
2026-07-30 10:52:20 +0530

    updating database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,47 @@
-const { Sequelize } = require('sequelize');
-const pg = require('pg');
+// const { Sequelize } = require('sequelize');
+// const pg = require('pg');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectModule: pg,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false 
-        }
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectModule: pg,
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false 
+//         }
+//     },
+//     pool: {
+//         max: 3,
+//         min: 0,
+//         idle: 10000,
+//         acquire: 30000
+//     }
+// });
+
+// module.exports = sequelize;
+
+
+
+
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false, 
+        dialectOptions: {
+           ssl: {
+               require: true,
+               rejectUnauthorized: false 
+           }
+       },
     },
-    pool: {
-        max: 3,
-        min: 0,
-        idle: 10000,
-        acquire: 30000
-    }
-});
+);
 
 module.exports = sequelize;
